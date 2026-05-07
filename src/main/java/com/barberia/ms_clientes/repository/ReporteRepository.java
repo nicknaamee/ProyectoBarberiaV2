5da5d45b0ba6d3e2694f89fc1b8df92ce90fac8a
Leanplzx <l.matias.a.vergara@gmail.com>
2026-05-07 19:29:17 -0400

    repositorios actualizados

--- src/main/java/com/barberia/ms_clientes/repository/ReporteRepository.java
+++ src/main/java/com/barberia/ms_clientes/repository/ReporteRepository.java
@@ -1,5 +1,11 @@
 package com.barberia.ms_clientes.repository;
 
-public class ReporteRepository {
+import com.barberia.ms_clientes.model.Reporte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface ReporteRepository extends JpaRepository<Reporte, Long> {
 }
